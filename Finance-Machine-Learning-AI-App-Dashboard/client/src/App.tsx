fd3188ffbafa813e4f881680c0b278311d9aa4b9
daryllculas <culasdaryll99@gmail.com>
2026-03-25 16:31:43 +0800

    feat: implement theme provider and baseline styles in App component

--- Finance-Machine-Learning-AI-App-Dashboard/client/src/App.tsx
+++ Finance-Machine-Learning-AI-App-Dashboard/client/src/App.tsx
@@ -1,7 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { ThemeProvider, CssBaseline } from "@mui/material";
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
   return (
-    <div className="app"></div>
+    <div className="app">
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </div>
   )
 }
 
